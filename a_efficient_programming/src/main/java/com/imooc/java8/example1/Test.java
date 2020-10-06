27a227e79fa60fd5f0e6c5b7eded9fdea83789f4
zhaobin <1903596027@qq.com>
2020-10-06 23:15:04 +0800

    Main -> Test

--- a_efficient_programming/src/main/java/com/imooc/java8/example1/Test.java
+++ a_efficient_programming/src/main/java/com/imooc/java8/example1/Test.java
@@ -3,7 +3,7 @@ package com.imooc.java8.example1;
 /**
  * @author zhaobin11@baidu.com
  */
-public class Main {
+public class Test {
 
     public static void fun(Animal animal) {
         System.out.println(1);
@@ -13,9 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
         Animal animal = (a) -> System.out.println(a);
-        Main.fun(animal);
+        Test.fun(animal);
         // 在java语言中，lambda表达式是对象而不是函数，必须依附于函数式接口而存在
-        System.out.println(animal.getClass()); // class Main$$Lambda$1/990368553
+        System.out.println(animal.getClass()); // class Test$$Lambda$1/990368553
         System.out.println(animal.getClass().getSuperclass()); // class java.lang.Object
         System.out.println(animal.getClass().getInterfaces()[0]); // interface Animal2
     }
